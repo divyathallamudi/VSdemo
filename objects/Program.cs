6b4d71ecc8866c43913d144a85afdc07130b56e5
Divya Thallamudi (Tech Mahindra Ltd) <v-dithal@microsoft.com>
2018-04-06 15:18:39 -0700

    Add project files.

---
+++ objects/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objects
+{
+    class Employee  /*type*/
+    {
+        //private int age; /*field*, ideally not public for encapsulation*/
+        //public int myage(int setage)
+        //{
+        //    age = setage;
+        //    return age;
+        //}
+        //public int Age
+        //{
+        //    get { return age; }
+        //    set { age = value; }
+        //        }
+        public int Age { get; set; } /*short form to above get, set and declaring variable age*/
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee Dave = new Employee(); /*creating instance of class*/
+            //Dave.age = 25;
+            //Console.WriteLine("Dave's age is {0}", D ave.age);
+            //Console.ReadLine();
+            Employee Dave = new Employee();
+            //int Daveage = Dave.myage(35);
+            //Console.WriteLine("Dave's age is {0}", Daveage);
+            //Console.ReadLine();
+            Dave.Age = 25;
+            Console.WriteLine("Dave's age is {0}", Dave.Age);
+            Console.ReadLine();
+        }
+    }
+}
